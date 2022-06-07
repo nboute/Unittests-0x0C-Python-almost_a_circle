8c49f3c2c417bb12e24b9e2e10aa7afa9bd274c3
comels <come.l.s@hotmail.fr>
2022-06-07 13:10:10 +0000

    docu come

--- tests/test_models/test_save_to_file.py
+++ tests/test_models/test_save_to_file.py
@@ -12,12 +12,14 @@ class TestBase_save_to_file(unittest.TestCase):
     """Unittests for testing save_to_file method of Base class."""
 
     def test_save_one_rectangle(self):
+        """test for one rectangle"""
         rect = Rectangle(10, 7, 2, 8, 5)
         Rectangle.save_to_file([rect])
         with open("Rectangle.json", "r") as file:
             self.assertTrue(len(file.read()) == 53)
 
     def test_save_two_rectangles(self):
+        """test for two rectangles"""
         rect1 = Rectangle(10, 7, 2, 8, 5)
         rect2 = Rectangle(2, 4, 1, 2, 3)
         Rectangle.save_to_file([rect1, rect2])
@@ -25,12 +27,14 @@ class TestBase_save_to_file(unittest.TestCase):
             self.assertTrue(len(file.read()) == 105)
 
     def test_save_one_square(self):
+        """test for one square"""
         square = Square(10, 7, 2, 8)
         Square.save_to_file([square])
         with open("Square.json", "r") as file:
             self.assertTrue(len(file.read()) == 39)
 
     def test_save_two_squares(self):
+        """test for two squares"""
         square1 = Square(10, 7, 2, 8)
         square2 = Square(8, 1, 2, 3)
         Square.save_to_file([square1, square2])
@@ -38,6 +42,7 @@ class TestBase_save_to_file(unittest.TestCase):
             self.assertTrue(len(file.read()) == 77)
 
     def test_save_overwrite(self):
+        """test for overwrite"""
         square = Square(8, 5, 9, 2)
         Square.save_to_file([square])
         square = Square(10, 7, 2, 8)
@@ -46,15 +51,18 @@ class TestBase_save_to_file(unittest.TestCase):
             self.assertTrue(len(file.read()) == 39)
 
     def test_save_empty_list(self):
+        """test for an empty list"""
         Square.save_to_file([])
         with open("Square.json", "r") as file:
             self.assertEqual("[]", file.read())
 
     def test_save_no_args(self):
+        """test for no argument"""
         with self.assertRaises(TypeError):
             Rectangle.save_to_file()
 
     def test_save_two_arg(self):
+        """test for two arguments"""
         with self.assertRaises(TypeError):
             Square.save_to_file([], 1)
 
--- tests/test_models/test_setter_height_width.py
+++ tests/test_models/test_setter_height_width.py
@@ -10,79 +10,95 @@ from models.base import Base
 class TestRectangle_setter_getter(unittest.TestCase):
     """test getter setter of the rectangle height and width"""
     def test_height_getter(self):
+        """test for the getter of the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         self.assertEqual(3, rect.height)
 
     def test_height_setter(self):
+        """test to set an int to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         rect.height = 12
         self.assertEqual(12, rect.height)
 
     def test_height_setter_negative(self):
+        """test to set a negative int to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(ValueError):
             rect.height = -12
 
     def test_height_setter_float(self):
+        """test to set a float to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
             rect.height = 1.2
 
     def test_height_setter_string(self):
+        """test to set a string to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
             rect.height = "ok"
 
     def test_height_setter_list(self):
+        """test to set a list to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
             rect.height = [12]
 
     def test_height_setter_tuple(self):
+        """test to set a tuple to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
             rect.height = (1, 2)
 
     def test_height_setter_dict(self):
+        """test to set a dictonary to the height"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
             rect.height = {12}
             
     def test_width_getter(self):
+        """test for the getter of the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         self.assertEqual(2, rect.width)
 
     def test_width_setter(self):
+        """test to set an int to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         rect.width = 12
         self.assertEqual(12, rect.width)
 
     def test_width_setter_negative(self):
+        """test to set a negative int to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(ValueError):
                 rect.width = -12
 
     def test_width_setter_float(self):
+        """test to set a float to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
                 rect.width = 1.2
 
     def test_width_setter_string(self):
+        """test to set a string to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
                 rect.width = "ok"
 
     def test_width_setter_list(self):
+        """test to set a list to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
                 rect.width = [12]
 
     def test_width_setter_tuple(self):
+        """test to set a tuple to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
                 rect.width = (1, 2)
 
     def test_width_setter_dict(self):
+        """test to set a dictionary to the width"""
         rect = Rectangle(2, 3, 4, 5, 6)
         with self.assertRaises(TypeError):
                 rect.width = {12}
